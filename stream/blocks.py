d30df1598a77676c61fc2861187ffa612f82a2bc
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-04 13:24:51 +0100

    Create Menu

---
+++ stream/blocks.py
@@ -0,0 +1,7 @@
+from wagtail import blocks
+from wagtail.fields import RichTextField
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.blocks import RichTextBlock
+
+
+# Marketing Block

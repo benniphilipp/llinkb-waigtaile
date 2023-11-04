942a98a90a282c6d4111a663e832358a5c13dc56
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-04 13:08:53 +0100

    Ceate Project

--- home/models.py
+++ home/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
+from wagtail.admin.panels import FieldPanel, ObjectList, TabbedInterface
 from wagtail.models import Page
 
-
 class HomePage(Page):
-    pass
+    template = "home/home_page.html"
+    max_count = 1
+    
+    # Hero Homepage
+    headline = models.CharField(blank=True, max_length=350, verbose_name="Überschrift")
+    subline = models.CharField(blank=True, max_length=350, verbose_name="Unterzeile")
+
+    # Hero Fields
+    content_panels = Page.content_panels + [
+        FieldPanel('headline'),
+        FieldPanel('subline'),
+    ]
+    
+    # Admin Tabs
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading='Header'),
+        ObjectList(Page.promote_panels, heading='Promotional'),
+        ObjectList(Page.settings_panels, heading='Settings'),
+    ])

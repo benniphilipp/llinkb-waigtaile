1a8f3832f1e16b82c7da34d49a60713823383762
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-05 19:58:43 +0100

    Update Local

---
+++ src/settings/local.py
@@ -0,0 +1,3 @@
+SECRET_KEY = "django-insecure-vksuz$!6vb)cyglv*(7mijr44d0x(%5#7xs7d@7n9zs=!s0re3"
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['llinkb.be', 'www.llinkb.be']

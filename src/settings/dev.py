c69c4391000254b6ccda257bc803ce5a54ed1339
root <benniph86@gmail.com>
2023-11-06 15:05:04 +0100

    Final Settings

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -10,7 +10,7 @@ SECRET_KEY = "django-insecure-vksuz$!6vb)cyglv*(7mijr44d0x(%5#7xs7d@7n9zs=!s0re3
 ALLOWED_HOSTS = ["localhost", "89.58.32.21:8000", "*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+CSRF_TRUSTED_ORIGINS = ["https://*.llinkb.be"]
 
 try:
     from .local import *

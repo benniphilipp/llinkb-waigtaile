5dc6c32e6d180589b32642af42d3df5dfce7f61c
ben_phi <33292239+benniphilipp@users.noreply.github.com>
2023-11-06 10:08:21 +0100

    Settings

--- src/settings/local.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = "django-insecure-vksuz$!6vb)cyglv*(7mijr44d0x(%5#7xs7d@7n9zs=!s0re3"
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['llinkb.be', 'www.llinkb.be']
--- src/settings/production.py
+++ src/settings/production.py
@@ -1,6 +1,24 @@
+from __future__ import absolute_import, unicode_literals
+import os
 from .base import *
 
+env = os.environ.copy()
+
 DEBUG = False
+SECRET_KEY = env['SECRET_KEY']
+ALLOWED_HOSTS=['llinkb.be', 'www.llinkb.be']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wagtail',
+        'USER': 'wagtail_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 
 
 try:
